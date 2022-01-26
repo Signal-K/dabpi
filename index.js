e98bc93ae00f257ba37fa28f35e9eca96ea9ac4c
Gizmotronn <droidology@allianceofdroids.org.au>
2022-01-26 23:40:10 +1100

    🧪🎉 Finishing work on Github auth project -> Moving to Unity cli

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
+const github = require('./lib/github');
 
 const files = require('./lib/files');
 
@@ -18,3 +19,16 @@ if (files.directoryExists('.git')) {
     console.log(chalk.red('Already a Git repository!'));
     process.exit();
 }
+
+// Authentication
+//const inquirer = require('./lib/inquirer');
+const run = async () => {
+    let token = github.getStoredGithubToken();
+    if(!token) {
+        token = await github.getPersonalAccessToken();    
+    }
+    console.log(token);
+    const credentials = await inquirer.askGithubCredentials();
+};
+
+run();
